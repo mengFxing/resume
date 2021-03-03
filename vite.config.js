2ee36a8a3fa545b4155536c046cf56879eac04c1
孟凡兴 <mengfanxing@vlifepaper.com>
2021-03-03 23:29:51 +0800

    添加环境变量区别

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 import findChrome from 'chrome-finder'
 // https://vitejs.dev/config/
 export default ({ command,mode }) =>{
+  const {
+    VITE_APP_PUBLIC_PATH,
+  } = loadEnv(mode, process.cwd())
+
+  const VITE_APP_IS_SERVE = command === 'serve' ? true : false;
   return defineConfig({
+    base: VITE_APP_IS_SERVE ? './' : VITE_APP_PUBLIC_PATH,
     plugins: [vue(),svgLoader()],
     define:{
       VITE_APP_TIME: Date.now()
