25cd1f7614937db5b19fa6dcfb203393d9b710d7
孟凡兴 <mengfanxing@vlifepaper.com>
2021-03-04 00:00:36 +0800

    修改文案

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+import { spawnSync } from 'child_process'
+import path from 'path'
 import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 import findChrome from 'chrome-finder'
+
+let outputPath = './'
+const buildPdf = ()=>{
+  return {
+    name:'buildPdf',
+    closeBundle:()=>{
+      const chromePath = findChrome();
+      spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
+        'https://mengfxing.github.io/resume/' // 这里注意改成你的在线简历的网站
+      ]);
+      
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default ({ command,mode }) =>{
   const {
@@ -9,9 +26,10 @@ export default ({ command,mode }) =>{
   } = loadEnv(mode, process.cwd())
 
   const VITE_APP_IS_SERVE = command === 'serve' ? true : false;
+  outputPath = VITE_APP_IS_SERVE ? './' : VITE_APP_PUBLIC_PATH
   return defineConfig({
-    base: VITE_APP_IS_SERVE ? './' : VITE_APP_PUBLIC_PATH,
-    plugins: [vue(),svgLoader()],
+    base: outputPath,
+    plugins: [vue(),svgLoader(),buildPdf()],
     define:{
       VITE_APP_TIME: Date.now()
     },
